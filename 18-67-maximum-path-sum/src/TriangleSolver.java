5b71feb65039939e507426b88711e288dee5c9a3
Matt Kendall <mattk91@gmail.com>
2016-11-12 13:05:27 +0000

    Add TriangleSolver interface

---
+++ 18-67-maximum-path-sum/src/TriangleSolver.java
@@ -0,0 +1,14 @@
+import model.Triangle;
+
+/**
+ * Finds routes through a triangle to calculate the maximum path sum.
+ */
+public interface TriangleSolver {
+
+    /**
+     * Solve for the maximum path sum through a triangle.
+     * @param triangle the triangle to solve for a route through
+     * @return the maximum path sum through the triangle
+     */
+    int findMaxPathSum(Triangle triangle);
+}
