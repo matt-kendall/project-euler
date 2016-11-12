d0d6612d10394c893e35cad74824b694f38b7bfa
Matt Kendall <mattk91@gmail.com>
2016-11-12 00:30:46 +0000

    Add Row and RowTest

---
+++ 18-67-maximum-path-sum/src/Row.java
@@ -0,0 +1,33 @@
+import exception.EmptyRowException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Represents one row of the Triangle.
+ */
+public class Row {
+
+    private final List<Point> points;
+
+    /**
+     * Create a new Row from a list of points.
+     * @param points points in the row
+     */
+    public Row(List<Point> points){
+        if (points.size() > 0) {
+            this.points = Collections.unmodifiableList(new ArrayList<>(points));
+        } else {
+            throw new EmptyRowException();
+        }
+    }
+
+    /**
+     * The list of points which make up this row.
+     * @return points in this row
+     */
+    public List<Point> points() {
+        return this.points;
+    }
+}
---
+++ 18-67-maximum-path-sum/test/RowTest.java
@@ -0,0 +1,71 @@
+import exception.EmptyRowException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for immutability and behaviour of the Row class.
+ */
+public class RowTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void canCreateRowWithOnePoint() {
+        new Row(Collections.singletonList(
+                new Point(1)
+        ));
+    }
+
+    @Test
+    public void canGetPoints() {
+        Row row = new Row(Arrays.asList(
+                new Point(1),
+                new Point(2)
+        ));
+        assertEquals(2, row.points().size());
+        assertEquals(1, row.points().get(0).value());
+        assertEquals(2, row.points().get(1).value());
+    }
+
+    @Test
+    public void createRowWithZeroPointsThrowsInvalidRowException() {
+        // A row with zero points doesn't make sense
+        expectedException.expect(EmptyRowException.class);
+        new Row(Collections.emptyList());
+    }
+
+    @Test
+    public void rowIsImmutableByConstructor() {
+        // Verify that altering the list originally passed to the Row constructor does not mutate the Row.
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(1));
+
+        Row row = new Row(points);
+
+        points.clear(); // Mutate the original list
+        assertEquals(1, row.points().size());
+    }
+
+
+    @Test
+    public void rowIsImmutableByPointsMethod() {
+        // Verify that altering the list returned by the points method does not mutate the Row (throws exception)
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(1));
+
+        Row row = new Row(points);
+
+        expectedException.expect(UnsupportedOperationException.class);
+        row.points().clear(); // Mutate the returned list
+    }
+
+}
