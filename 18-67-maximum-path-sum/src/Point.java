ec6a013bfa4636620bdd0fbcbcff363c7b18586f
Matt Kendall <mattk91@gmail.com>
2016-11-12 00:08:50 +0000

    Add point class

---
+++ 18-67-maximum-path-sum/src/Point.java
@@ -0,0 +1,23 @@
+/**
+ * Represents a point in a number triangle.
+ */
+public class Point {
+
+    private final int value;
+
+    /**
+     * Create new point of specified value.
+     * @param value value of point
+     */
+    public Point(int value) {
+        this.value = value;
+    }
+
+    /**
+     * The numerical value of this point.
+     * @return point value
+     */
+    public int value() {
+        return value;
+    }
+}
