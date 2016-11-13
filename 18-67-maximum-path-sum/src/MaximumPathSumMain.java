5649617bbdbdef7a5d93e9ac5564bb77d3d459e4
Matt Kendall <mattk91@gmail.com>
2016-11-13 14:01:22 +0000

    Add main class (currently only solve PE18)

---
+++ 18-67-maximum-path-sum/src/MaximumPathSumMain.java
@@ -0,0 +1,36 @@
+import io.FileLoader;
+import io.TriangleLoader;
+import model.Triangle;
+import routing.PossibleRouteFinder;
+import solvers.BruteForceTriangleSolver;
+import solvers.TriangleSolver;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Main entry class for solving Project Euler exercises 18 and 67.
+ */
+public class MaximumPathSumMain {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        solve18BruteForce();
+    }
+
+    private static void solve18BruteForce() throws FileNotFoundException {
+        System.out.println("Solving Project Euler puzzle 18...");
+        Triangle triangle = getTriangle("triangle-15-rows.txt");
+
+        TriangleSolver solver = new BruteForceTriangleSolver(new PossibleRouteFinder());
+        solveAndOutput(triangle, solver);
+    }
+
+    private static void solveAndOutput(Triangle triangle, TriangleSolver solver) {
+        int maxPathSum = solver.findMaxPathSum(triangle);
+        System.out.println(String.format("Maximum Path Sum is %d", maxPathSum));
+    }
+
+    private static Triangle getTriangle(String filename) throws FileNotFoundException {
+        TriangleLoader triangleLoader = new TriangleLoader(new FileLoader());
+        return triangleLoader.loadFile(filename);
+    }
+}
